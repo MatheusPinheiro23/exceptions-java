46caafba8d72291dd2c1c83794a1c9222df43a25
Matheus Pinheiro <matheus.pinheiro1996@hotmail.com>
2021-02-09 12:45:35 -0300

    Good solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,15 +4,20 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
-
+	
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
+	
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -36,33 +41,31 @@ public class Reservation {
 
 	public long duration() {
 		long diff = checkOut.getTime() - checkIn.getTime();
-		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);// converte o valor de milli segundos para dias e
-																	// retorna esse valor
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-
-	public String updateDates(Date checkIn, Date checkOut) {
-
+	
+	public void updateDates(Date checkIn, Date checkOut) {
 		Date now = new Date();
-
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "Reservation dates for update must be future dates";
-		} 
-
-		if (!checkOut.after(checkIn)) {
-			return "Check-out date must be after check-in date";
+			throw new DomainException("Reservation dates for update must be future dates");
 		}
-
-		this.checkIn = checkIn; // atualiza as datas de CheckIn
-		this.checkOut = checkOut; // atualiza as datas de CheckOut
-		return null; // se retornar nulo é porque não deu nenhum erro
-
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
 	}
-
+	
 	@Override
 	public String toString() {
-		return "Room " + roomNumber + ", check-in: " + sdf.format(checkIn) + ", check-out: " + sdf.format(checkOut)
-				+ ", " + duration() + " nights";
-
+		return "Room "
+			+ roomNumber
+			+ ", check-in: "
+			+ sdf.format(checkIn)
+			+ ", check-out: "
+			+ sdf.format(checkOut)
+			+ ", "
+			+ duration()
+			+ " nights";
 	}
-
 }
